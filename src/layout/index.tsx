22efd1c617ddeb95a2c852a8a10001643dad4004
Lawrence Yi-Hsuan Chuang <green760223@gmail.com>
2024-02-06 01:01:32 -0800

    Implement watermark background on the dashboard page

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {
   UploadOutlined,
   UserOutlined,
   VideoCameraOutlined
 } from '@ant-design/icons'
-import { Layout, Menu, theme } from 'antd'
+import { Layout, Menu, theme, Watermark } from 'antd'
 
 const { Header, Content, Footer, Sider } = Layout
 
@@ -24,41 +24,63 @@ const App: React.FC = () => {
     token: { colorBgContainer, borderRadiusLG }
   } = theme.useToken()
 
+  useEffect(() => {
+    const targetNode = document.getElementById('content') as HTMLDivElement
+    const observer = new MutationObserver(function (mutationList, observer) {
+      console.log(mutationList, observer)
+      console.log('content changed')
+      observer.disconnect()
+      for (const mutation of mutationList) {
+        if (mutation.type === 'childList') {
+          const span = document.createElement('span')
+          span.innerText = 'Hello React'
+          targetNode.appendChild(span)
+          observer.observe(targetNode, config)
+        }
+      }
+    })
+    const config = { attributes: true, childList: true, subtree: true }
+    observer.observe(targetNode, config)
+  }, [])
+
   return (
-    <Layout>
-      <Sider
-        breakpoint='lg'
-        collapsedWidth='0'
-        onBreakpoint={broken => {
-          console.log(broken)
-        }}
-        onCollapse={(collapsed, type) => {
-          console.log(collapsed, type)
-        }}
-      >
-        側邊欄
-      </Sider>
+    <Watermark content='React'>
       <Layout>
-        <Header style={{ padding: 0, background: colorBgContainer }}>
-          頂部區域
-        </Header>
-        <Content style={{ margin: '24px 16px 0' }}>
-          <div
-            style={{
-              padding: 24,
-              minHeight: 360,
-              background: colorBgContainer,
-              borderRadius: borderRadiusLG
-            }}
-          >
-            content
-          </div>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>
-          Ant Design ©{new Date().getFullYear()} Created by Ant UED
-        </Footer>
+        <Sider
+          breakpoint='lg'
+          collapsedWidth='0'
+          onBreakpoint={broken => {
+            console.log(broken)
+          }}
+          onCollapse={(collapsed, type) => {
+            console.log(collapsed, type)
+          }}
+        >
+          側邊欄
+        </Sider>
+        <Layout>
+          <Header style={{ padding: 0, background: colorBgContainer }}>
+            頂部區域
+          </Header>
+          <Content style={{ margin: '24px 16px 0' }}>
+            <div
+              style={{
+                padding: 24,
+                minHeight: 360,
+                background: colorBgContainer,
+                borderRadius: borderRadiusLG
+              }}
+              id='content'
+            >
+              <span>content</span>
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>
+            Ant Design ©{new Date().getFullYear()} Created by Ant UED
+          </Footer>
+        </Layout>
       </Layout>
-    </Layout>
+    </Watermark>
   )
 }
 
