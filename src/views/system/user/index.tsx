7380be2dd5bcf7b2399c5b98933736511225c512
Lawrence Yi-Hsuan Chuang <green760223@gmail.com>
2024-02-19 02:20:16 -0800

    Add layout for user list page with data loading functionality

--- src/views/system/user/index.tsx
+++ src/views/system/user/index.tsx
@@ -1,44 +1,100 @@
-import { Button, Table, Form, Input } from 'antd'
+import { User } from '@/types/api'
+import { Button, Table, Form, Input, Select, Space } from 'antd'
+import type { ColumnsType } from 'antd/es/table'
 
 function UserList() {
   const dataSource = [
     {
-      key: '1',
-      name: '胡彦斌',
-      age: 32,
-      address: '西湖区湖底公园1号'
-    },
-    {
-      key: '2',
-      name: '胡彦祖',
-      age: 42,
-      address: '西湖区湖底公园1号'
+      _id: '',
+      userId: 0,
+      userName: '',
+      userEmail: '',
+      deptId: '',
+      deptName: '',
+      state: 0,
+      mobile: '',
+      job: '',
+      role: 0,
+      roleList: '',
+      createId: 0,
+      userImg: ''
     }
   ]
 
-  const columns = [
+  const columns: ColumnsType<User.UserItem> = [
     {
-      title: '姓名',
-      dataIndex: 'name',
-      key: 'name'
+      title: '用戶ID',
+      dataIndex: 'userId',
+      key: 'userId'
     },
     {
-      title: '年龄',
-      dataIndex: 'age',
-      key: 'age'
+      title: '用戶名稱',
+      dataIndex: 'userName',
+      key: 'userName'
     },
     {
-      title: '住址',
+      title: '用戶信箱',
+      dataIndex: 'userEmail',
+      key: 'userEmail'
+    },
+    {
+      title: '用戶角色',
       dataIndex: 'address',
       key: 'address'
+    },
+    {
+      title: '用戶狀態',
+      dataIndex: 'state',
+      key: 'state'
+    },
+    {
+      title: '註冊時間',
+      dataIndex: 'createTime',
+      key: 'createTime'
+    },
+    {
+      title: '操作',
+      dataIndex: 'address',
+      key: 'address',
+      render() {
+        return (
+          <Space>
+            <Button type='text'>編輯</Button>
+            <Button type='text' danger>
+              刪除
+            </Button>
+          </Space>
+        )
+      }
     }
   ]
 
   return (
     <div className='user-list'>
-      <Form className='search-form' layout='inline'>
+      <Form
+        className='search-form'
+        layout='inline'
+        initialValues={{ state: 1 }}
+      >
         <Form.Item name='userId' label='用戶ID'>
-          <Input />
+          <Input placeholder='請輸入用戶ID' />
+        </Form.Item>
+        <Form.Item name='userName' label='用戶名稱'>
+          <Input placeholder='請輸入用戶名稱' />
+        </Form.Item>
+        <Form.Item name='state' label='狀態'>
+          <Select style={{ width: 120 }}>
+            <Select.Option value={0}>所有</Select.Option>
+            <Select.Option value={1}>在職</Select.Option>
+            <Select.Option value={2}>試用期</Select.Option>
+            <Select.Option value={3}>離職</Select.Option>
+          </Select>
+        </Form.Item>
+        <Form.Item name='search' label='搜尋'>
+          <Space>
+            <Button type='primary'>搜尋</Button>
+            <Button type='default'>重置</Button>
+          </Space>
         </Form.Item>
       </Form>
       <div className='base-table'>
