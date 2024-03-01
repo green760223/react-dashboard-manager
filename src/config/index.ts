368d79a4039431634b088b7bbda4f9016a94020f
Lawrence Yi-Hsuan Chuang <green760223@gmail.com>
2024-03-01 12:05:13 -0800

    Update files

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,7 +7,7 @@ const env = (document.documentElement.dataset.env as ENV) || 'stg'
 
 const config = {
   dev: {
-    baseAPI: 'http://api-driver-dev.marsview.cc/api',
+    baseAPI: '/api',
     uploadAPI: 'http:///api-driver-dev.marsview.cc',
     mock: false,
     cnd: 'http://www.aliyun.com',
@@ -15,7 +15,7 @@ const config = {
       'https://www.fastmock.site/mock/a5c0902ecbc737f8e4d65447420fe0d2/api'
   },
   stg: {
-    baseAPI: 'http://api-driver-stg.marsview.cc/api',
+    baseAPI: '/api',
     uploadAPI: 'http:///api-driver-stg.marsview.cc',
     mock: false,
     cnd: 'http://www.aliyun.com',
