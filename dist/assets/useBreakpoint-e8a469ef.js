c90da57382d4f16a0fbc7b5df22d67670e3ebf2f
Lawrence Yi-Hsuan Chuang <green760223@gmail.com>
2024-03-29 18:07:53 -0700

    Update config

--- dist/assets/useBreakpoint-e8a469ef.js
+++
@@ -1,1 +0,0 @@
-import{r as t,d0 as c,at as a}from"./index-b88df3f9.js";function f(){const[,r]=t.useReducer(e=>e+1,0);return r}function p(){let r=arguments.length>0&&arguments[0]!==void 0?arguments[0]:!0;const e=t.useRef({}),n=f(),s=c();return a(()=>{const u=s.subscribe(o=>{e.current=o,r&&n()});return()=>s.unsubscribe(u)},[]),e.current}export{f as a,p as u};
