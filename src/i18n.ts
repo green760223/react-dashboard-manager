0be47924764298b2a2a405f9efa744a96c035502
Lawrence Yi-Hsuan Chuang <green760223@gmail.com>
2024-06-12 18:18:37 -0700

    Update config

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,7 +1,5 @@
 import { initReactI18next } from 'react-i18next'
 import i18n from 'i18next'
-import HttpBackend from 'i18next-http-backend'
-import LanguageDetector from 'i18next-browser-languagedetector'
 import translationEN from './locales/en/translation.json'
 import translationZH from './locales/zh/translation.json'
 
