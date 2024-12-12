b15a680fac6f0d488d23b9f606f50b95fad675c2
Lawrence Yi-Hsuan Chuang <green760223@gmail.com>
2024-12-12 21:43:15 +0800

    Update file

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -16,7 +16,7 @@ const instance = axios.create({
   timeoutErrorMessage: 'Request timed out. Please try again later！',
   withCredentials: true, // 允許夾帶cookie
   headers: {
-    icode: 'DDBC40EE298DAE1E'
+    icode: '5449DB59DE516E2B'
   }
 })
 
