4fecd6c9f43ca05ac83ee701a17042143e786149
Lawrence Yi-Hsuan Chuang <green760223@gmail.com>
2025-04-11 03:21:39 +0800

    update api proxy service endpoint and correct 'icode' in request headers

--- src/config/index.ts
+++ src/config/index.ts
@@ -24,8 +24,8 @@ const config = {
       'https://www.fastmock.site/mock/a5c0902ecbc737f8e4d65447420fe0d2/api'
   },
   prod: {
-    baseAPI: 'https://api-proxy-service-128315aa4d53.herokuapp.com/api',
-    uploadAPI: 'https://api-proxy-service-128315aa4d53.herokuapp.com/',
+    baseAPI: 'https://api-proxy-service-production.up.railway.app/api',
+    uploadAPI: 'https://api-proxy-service-production.up.railway.app/',
     mock: false,
     cnd: 'http://www.aliyun.com',
     mockAPI:
