8c6c0b4d49c044f4c5f51ebf60ffe3d05dae1843
Lawrence Yi-Hsuan Chuang <green760223@gmail.com>
2025-06-16 23:43:40 -0700

    Update config

--- src/config/index.ts
+++ src/config/index.ts
@@ -26,7 +26,7 @@ const config = {
       'https://www.fastmock.site/mock/a5c0902ecbc737f8e4d65447420fe0d2/api'
   },
   prod: {
-    baseAPI: '/api',
+    baseAPI: 'https://api-driver.marsview.com.cn',
     uploadAPI: 'https://api-driver.marsview.com.cn',
     mock: false,
     cdn: 'http://www.aliyun.com',
